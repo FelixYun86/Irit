887fa7f847cac7176a431b841cf0f55fc07ea356
Felix <felix.yuniar@gmail.com>
2019-10-24 07:34:55 +0700

    Initial Commit

---
+++ code/canvas.tsx
@@ -0,0 +1,6 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Component = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_fdQ1_PYOU", {}, 50,28);
+export const Component_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_jXKuOB9O6", {}, 326,48);
+export const Tag = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Label?:string}>(canvas, "id_VtsefEk1L", {Label:"string"}, 68,32);
